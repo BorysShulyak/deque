b1dbbc7ad2878605c7f85ff59775e46ce2ecc964
BorysShulyak <borysshulyak@gmail.com>
2021-11-28 16:16:11 +0200

    Add ItemInitialization test.

---
+++ DequeLibrary.Test/ItemTests.cs
@@ -0,0 +1,26 @@
+﻿using DequeLibrary;
+using Xunit;
+
+namespace DequeLibrary.Test
+{
+    public class ItemTests
+    {
+        [Fact]
+        public void ItemInitialization()
+        {
+            // Arrange
+            int mockedData = 10;
+            Item<int> item = new Item<int>(mockedData);
+
+            // Act
+            int itemData = item.Data;
+            Item<int> nextItem = item.Next;
+            Item<int> previousItem = item.Previous;
+
+            // Assert
+            Assert.Equal(mockedData, item.Data);
+            Assert.Null(nextItem);
+            Assert.Null(previousItem);
+        }
+    }
+}
